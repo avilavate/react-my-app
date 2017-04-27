27222f580cb852906bd9a2b4f16bca84b70e20c9
avilavate <avilavate123@gmail.com>
2017-04-27 12:03:29 +0530

    Adding and rending multiple components.

---
+++ src/Danger.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Danger extends Component {
+    render() {
+        return <button className="App-Danger">Danger</button>;
+    }
+}
+
+export default Danger;
